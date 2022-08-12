4639457e173068a43fa5cb8c2bdfd476026561c4
LucasVena <lsvena@hotmail.com>
2022-08-12 19:21:09 -0300

    Crear la App utilizando el CLI

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='queonda'>
+          Hola padawans a darde duro a React JS!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          A por ello!
         </a>
       </header>
     </div>
